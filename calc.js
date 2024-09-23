097f062206090f2e5d57f22fa88f87c722c6ef6f
Sathish N <itssathishneelakandan@gmail.com>
2024-09-23 12:13:51 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+
+// Function to append values to the display
+function appendValue(value) {
+    display.value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    display.value = '';
+}
+
+// Function to delete the last digit
+function deleteDigit() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Function to calculate the result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
